ec55a7793669a8410906f778c7ee02132852847c
Janidu Gamlath <gamlath18@gmail.com>
2025-06-10 19:11:40 +0530

    Update HelloWorld.java
    Add 2 more Hello world messages

--- my-first-pipeline/HelloWorld.java
+++ my-first-pipeline/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
         System.out.println("Hello, World!");
          System.out.println("Hello, World! again");
         System.out.println("Hello, World! again");
+          System.out.println("Hello, World! again");
+        System.out.println("Hello, World! again");
     }
 }
